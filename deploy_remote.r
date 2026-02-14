2b9dec6b924727020b880d1eefefab38fc164fd3
Zhenhua Wang <wang_zhenhua1996@icloud.com>
2026-02-14 14:17:16 -0600

    Remove deploy remote

--- deploy_remote.r
+++
@@ -1,8 +0,0 @@
-library(rsconnect)
-
-rsconnect::setAccountInfo(
-  name='zhenhua-wang-mizzou',
-  token='EB426C9E39B7ABE180F65D9F7CAD3ED5',
-  secret='/nMkOe+eNFd5GDHE1/lHXRlX+iRPWBmark1rw95G')
-
-rsconnect::deployApp('~/Workspace/Hackthon/traffic_stop/')
